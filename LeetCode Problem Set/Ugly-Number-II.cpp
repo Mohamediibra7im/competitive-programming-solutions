18d642bca349cb64f720dd5c6b6301640b8163d0
Mohammed Ibrahim <163002011+Mohamediibra7im@users.noreply.github.com>
2025-08-22 14:40:07 +0300

    [264] [Time Beats: 100.00%] [Memory Beats: 98.18%] - LeetPush

---
+++ LeetCode Problem Set/Ugly-Number-II.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> ugly(n);
+        ugly[0] = 1;
+        // Pointers to the next multiple of 2, 3, 5
+        int i2 = 0, i3 = 0, i5 = 0;
+
+        // Next multiples of 2, 3, 5
+        int next_multiple_of_2 = 2, next_multiple_of_3 = 3,
+            next_multiple_of_5 = 5;
+
+        for (int i = 1; i < n; i++) {
+            int next_ugly_number = min(
+                {next_multiple_of_2, next_multiple_of_3, next_multiple_of_5});
+            ugly[i] = next_ugly_number;
+
+            if (next_ugly_number == next_multiple_of_2) {
+                i2++;
+                next_multiple_of_2 = ugly[i2] * 2;
+            }
+            if (next_ugly_number == next_multiple_of_3) {
+                i3++;
+                next_multiple_of_3 = ugly[i3] * 3;
+            }
+            if (next_ugly_number == next_multiple_of_5) {
+                i5++;
+                next_multiple_of_5 = ugly[i5] * 5;
+            }
+        }
+
+        return ugly[n - 1];
+    }
+};
